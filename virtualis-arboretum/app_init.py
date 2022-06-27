8af52b9176abe162db3cbf059f3299dc99c8e429
Zsofia Limbek <zsofia.limbek@codecool.com>
2022-06-27 17:34:46 +0200

    deleted remaining swagger-gen code

--- virtualis-arboretum/app_init.py
+++ virtualis-arboretum/app_init.py
@@ -3,7 +3,6 @@ import logging
 from flask import Flask
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
-from swagger_gen import swagger
 
 logging.basicConfig(format="%(asctime)s [%(levelname)s] %(name)s: %(message)s", level=logging.INFO)
 
@@ -17,14 +16,3 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
-
-
-def init_swagger(flask_app):
-    swagger_instance = swagger.Swagger(
-        app=flask_app,
-        title="Virtuális Arborétum",
-        version="0.0.1",
-        license_name="MIT License",
-        license_url="https://mit-license.org/"
-    )
-    return swagger_instance
