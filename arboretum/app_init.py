3a362283a6dac6867c11bec01cb13e42d94413a0
Zsofia Limbek <zsofia.limbek@codecool.com>
2022-06-27 20:19:02 +0200

    configured db uri based on environment variable

--- arboretum/app_init.py
+++ arboretum/app_init.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 from flask import Flask
 from flask_marshmallow import Marshmallow
@@ -6,14 +7,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 logging.basicConfig(format="%(asctime)s [%(levelname)s] %(name)s: %(message)s", level=logging.INFO)
 
-DB_NAME = "arboretum_dev"
-
 # Debug mode base url config
 HOST = "127.0.0.1"
 PORT = 5050
 
 app = Flask("virtualis_arboretum", static_folder="arboretum/static", template_folder="arboretum/templates")
-app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://postgres:postgres@localhost:5432/{DB_NAME}"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", default="Database URI not specified")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
